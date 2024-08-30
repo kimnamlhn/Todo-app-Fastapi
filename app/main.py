8cd98ecd9b4b4915e35215f8c135ddf2ee749f2a
Nam Le Hoai <45734413+kimnamlhn@users.noreply.github.com>
2024-08-30 15:43:14 +0700

    add get all users endpoint

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 # app.include_router(company.router)
 # app.include_router(task.router)
-# app.include_router(user.router)
-# app.include_router(auth.router)
+app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/", tags=["Health Check"])
 async def health_check():
